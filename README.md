634117019075afa3dcb7ad414e1de5cc34b9df55
Tanja Breitwieser <tanja.breitwieser@ibmix.at>
2023-06-22 16:20:58 +0200

    project commit tryout

---
+++ README.md
@@ -0,0 +1,13 @@
+# Webpack Loaders configuration DEMO
+
+To execute example, please first execute `npm install` within this folder.
+
+For development mode, execute `npm start` or `npm run start`
+
+For production mode, execute `npm run build`
+
+Watch the generated file within the dist folder how it change with the different modes.
+
+## The exercise
+
+See comments in index.js
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      }
+      /* insert image loader config here, all images should be outputted to directory 'asset/resources/images' */
+      /* second task involves also a loader dealing with html, here would be a good place for it */
+    ]
+  },
+  output: {
+    clean: true
+  },
+  plugins: [ new MiniCssExtractPlugin()]
+};
